31ae9422bbfae15dfe6a166f4338c0e7227d0519
David Schoemehl <davidschoemehl@gmail.com>
2016-07-09 21:39:54 -0500

    Adding new rendering

---
+++ TaskController.py
@@ -0,0 +1,16 @@
+#Responsible for handling mouse input and setting destinations on the tasks
+# Mouse Handlers
+class TaskController():
+
+    def on_mouse_press(self,x, y, button, modifiers):
+        print 'mouse pressed'
+        pass
+
+    def on_mouse_release(self,x, y, button, modifiers):
+        print 'mouse released'
+        pass
+
+    def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
+        print 'mouse dragged'
+        pass
+
